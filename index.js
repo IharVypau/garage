55311985436fecaf80c583195fad59fc405e072c
Ihar Vypau <igor.vypov@gmail.com>
2018-08-21 17:43:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+//Dependencies
+
+const server = require('./lib/server');
+let workers = require('./lib/workers');
+
+
+let app = {};
+
+app.init = () =>{
+    server.init();
+
+    workers.init();
+};
+
+
+app.init();
+
+module.exports = app;
